08b8e85eeb97156864011e1fa2fdc29e0443aa33
ac7x <ac7x@pm.me>
2025-05-04 20:48:34 +0800

    新增 C-Stock 與 C-Tag 介面層，包含 hooks、元件及工具函式的匯出，並提供用法說明與使用範例

---
+++ src/modules/c-tag/interfaces/index.ts
@@ -0,0 +1,28 @@
+/**
+ * C-Tag Interface Layer
+ * 
+ * 用法說明：
+ * - 建議於 presentation 層（如 page、component）直接 import 本層 hooks/components
+ * - hooks 只負責資料取得/快取/觸發 Server Action，無業務邏輯
+ * - 元件僅負責 UI 呈現與互動
+ * - 請勿於本層寫入任何業務邏輯，所有業務規則應封裝於 domain/application 層
+ */
+
+export * from './hooks/useTag';
+export * from './hooks/useTagMutations';
+export * from './hooks/useTags';
+export * from './hooks/useTagsByType';
+
+export * from './components/tag-card';
+export * from './components/tag-category-list';
+export * from './components/tag-edit-form';
+export * from './components/tag-form';
+export * from './components/tag-list';
+export * from './components/tag-type-filter';
+
+export * from './utils/tag-display-utils';
+
+// 使用範例：
+// import { useTags, TagList } from '@/modules/c-tag/interfaces';
+// const { data } = useTags();
+// <TagList tags={data ?? []} />
